7d2cd56f996456895cc29bb0b0df0a681add98a1
DavidBystroem <47114105+DavidBystroem@users.noreply.github.com>
2024-01-28 11:02:45 +0100

    Explosion stuff

--- Object/Object.gd
+++ Object/Object.gd
@@ -1,9 +1,16 @@
 extends RigidBody2D
 @export var objectScore := 0
+@export var animationtoplay := ""
+@export var goaway := ""
+var timesplayed := 1
+@onready var sprite = $Sprite2D/AnimationPlayer
 
 func _on_area_2d_area_entered(area):
 	if area.name == "Floor":
-		queue_free()
+		if timesplayed >= 0:
+			sprite.play(animationtoplay)
+			timesplayed = 0
+
 
 func _on_area_2d_3_body_entered(body):
 	if body.name == "PlayerCat":
@@ -15,3 +22,9 @@ func _on_area_2d_body_entered(body):
 	if body.name == "PlayerCat":
 		SignalBus.Stun_Player.emit()
 		queue_free()
+
+
+func _on_animation_player_animation_finished(anim_name):
+	print("ergio")
+	print(anim_name)
+	queue_free()
